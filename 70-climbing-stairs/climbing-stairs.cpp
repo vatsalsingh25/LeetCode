3cc863d2692f17d28ba047d8f7077ec6e653f354
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-07-24 15:42:03 +0530

    Time: 3 ms (29.48%) | Memory: 7.6 MB (10.65%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,13 +3,9 @@ public:
     int func(vector<int>&dp, int ind){
         if(ind==0) return 1;
         if(ind==1) return 1;
-
         if(dp[ind]!=-1) return dp[ind];
-
         int singleStep = func(dp,ind-1);
-        
         int doubleStep = func(dp,ind-2);
-
         return dp[ind] = singleStep+doubleStep;
     }
     int climbStairs(int ind) {
