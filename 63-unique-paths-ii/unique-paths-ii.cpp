04261c7ab7ad358d85e62501bb3461a6cb2a4bb6
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-07-26 21:27:42 +0530

    Time: 6 ms (21.11%) | Memory: 10.5 MB (11.80%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -4,12 +4,9 @@ public:
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
         if(obstacleGrid[i][j]==1) return 0; 
-
         if(dp[i][j]!=-1) return dp[i][j];
-
         int left = func(i,j-1,obstacleGrid,dp);
-        int up = func(i-1,j,obstacleGrid,dp);
-        
+        int up = func(i-1,j,obstacleGrid,dp); 
         return dp[i][j]=left+up;
     }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
