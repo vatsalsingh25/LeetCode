c770260977474e2b5a2dd1e3466a73a6553b0fd2
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-07-28 18:53:32 +0530

    Time: 176 ms (36.47%) | Memory: 11.1 MB (72.11%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int func(vector<int>nums, int ind, int target, vector<unordered_map<int,int>>&dp){
+    int func(vector<int>&nums,int ind, int sum){
         if(ind==0){
-            if(target==nums[0] && nums[0]==0) return 2;
-            if(target==nums[0] || target == -nums[0]) return 1;
-            else return 0;
+            if(sum==0 && nums[0]==0) return 2;
+            if(sum==0) return 1;
+            if(sum==nums[0]) return 1;
+            return 0;
         }
-        if(dp[ind].find(target) != dp[ind].end()) return dp[ind][target]; //negetive values of target cant be stored
-        int positive = func(nums,ind-1,target-nums[ind],dp);
-        int negative = func(nums, ind-1, target+nums[ind],dp);
+        
+        int notPick = func(nums,ind-1,sum);
+        int pick = 0;
+        if(sum>=nums[ind]) pick = func(nums,ind-1,sum-nums[ind]);
 
-        return dp[ind][target]=positive + negative;
+        return pick+notPick;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<unordered_map<int,int>>dp(n);
-        return func(nums,n-1,target,dp);
+       int n = nums.size();
+       int total=0;
+       for(int i=0; i<n; i++){
+            total+=nums[i];
+       } 
+
+        if((total-target)%2==1) return 0;
+        if(total-target<0) return 0;
+
+        return func(nums,n-1,(total-target)/2);
     }
+    //s1-s2=k
+    //s1=total-s2
+    //total-s2-s2=k
+    //s2=(total-k)/2
 };
