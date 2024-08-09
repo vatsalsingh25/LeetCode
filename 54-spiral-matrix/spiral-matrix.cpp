6325455a5db6be771434148d143e5c7c49689ef2
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-08-09 23:39:18 +0530

    Time: 3 ms (37.53%) | Memory: 8.5 MB (24.47%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,61 +1,37 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
         vector<int>ans;
-        int x = 0;
-        int y = 0;
-        int hlast = n;
-        int vlast = m;
-        int hfirst = 0;
-        int vfirst = 1;
-        while(1){
-            while(y<hlast){
-                ans.push_back(matrix[x][y]);
-                y++;
-                if(y==hlast){
-                    hlast-=1;
-                    y--;
-                    x++;
-                }
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        int left = 0;
+        int right = m-1;
+        int top = 0;
+        int bottom = n-1;
+        int num = 1;
+        while(left <= right && top <= bottom){              //imp
+            for(int i=left; i<=right; i++){
+                ans.push_back(matrix[top][i]);
             }
+            top++;
             if(ans.size()==n*m) break;
-            while(x<vlast){
-                ans.push_back(matrix[x][y]);
-                x++;
-                if(x==vlast){
-                    vlast-=1;
-                    x--;
-                    y--;
-                }
+            for(int i=top; i<=bottom; i++){
+                ans.push_back(matrix[i][right]);
             }
+            right--;
             if(ans.size()==n*m) break;
-            while(y>=hfirst){
-                ans.push_back(matrix[x][y]);
-                y--;
-                if(y==hfirst-1){
-                    hfirst+=1;
-                    y++;
-                    x--;
-                }
+            for(int i=right; i>=left; i--){
+                ans.push_back(matrix[bottom][i]);
             }
+            bottom--;
             if(ans.size()==n*m) break;
-            while(x>=vfirst){
-                ans.push_back(matrix[x][y]);
-                x--;
-                if(x==vfirst-1){
-                    vfirst+=1;
-                    x++;
-                    y++;
-                }
+            for(int i=bottom; i>=top; i--){
+                ans.push_back(matrix[i][left]);
             }
+            left++;  
             if(ans.size()==n*m) break;
         }
         return ans;
     }
 };
-// [ 1, 2, 3, 4],
-// [ 5, 6, 7, 8],
-// [ 9,10,11,12],
-// [13,14,15,16]...
