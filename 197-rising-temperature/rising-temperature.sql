7fc71162b9c66cd988cf006aace5ad254d6afe3e
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-08-21 11:18:18 +0530

    Time: 425 ms (60.11%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,5 +2,5 @@
 SELECT a.id 
 FROM Weather AS a
 JOIN Weather AS b
-ON DATEDIFF(a.recordDate,b.recordDate)=1 AND a.temperature>b.temperature;
+WHERE DATEDIFF(a.recordDate,b.recordDate)=1 AND a.temperature>b.temperature;
 
