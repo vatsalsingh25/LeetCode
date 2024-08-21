f62965876eac839d61dbf90274baaf353f2f470d
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-08-21 10:04:59 +0530

    Time: 1291 ms (52.68%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,9 +1,13 @@
-# Write your MySQL query statement below
+
+-- query to extract all values with no relatoins in second table(left join-b values)
+
 -- SELECT Visits.visit_id,costumer_id
 -- FROM Visits as a
 -- LEFT JOIN Transactions as b
 -- USING(visit_id)
 -- WHERE b.visit_id IS NULL;
+
+-- query to calculate the count and group by value(embed inside the from statement)
 
 SELECT customer_id, COUNT(visit_id) AS count_no_trans
 FROM (  SELECT visit_id,customer_id
