6cafa0b546dc6285ce35870a93dddc2ffa6d2ecd
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-08-21 10:22:20 +0530

    Time: 1136 ms (85.72%) | Memory: 0B (100.00%) - LeetSync

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,19 +1,7 @@
-
--- query to extract all values with no relatoins in second table(left join-b values)
-
--- SELECT Visits.visit_id,costumer_id
--- FROM Visits as a
--- LEFT JOIN Transactions as b
--- USING(visit_id)
--- WHERE b.visit_id IS NULL;
-
--- query to calculate the count and group by value(embed inside the from statement)
-
 SELECT customer_id, COUNT(visit_id) AS count_no_trans
-FROM (  SELECT visit_id,customer_id
-        FROM Visits AS a
-        LEFT JOIN Transactions as b
-        USING(visit_id)
-        WHERE b.visit_id IS NULL) AS no_transaction
+FROM Visits as a 
+LEFT JOIN Transactions as b
+USING(visit_id)
+WHERE b.visit_id IS NULL
 GROUP BY customer_id;
 
