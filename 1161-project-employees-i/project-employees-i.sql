0795df947f8ce8d423e89c87b86a21d4a36c9e74
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-08-27 02:19:35 +0530

    Time: 630 ms (18.01%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -6,8 +6,8 @@
 -- GROUP BY project_id;
 -- correct just needs to be rounded off
 
-SELECT project_id, ROUND(AVG(experience_years),2) as average_years
-FROM Project 
-LEFT JOIN Employee 
-USING(employee_id)
-GROUP BY project_id;
+SELECT p.project_id, ROUND(AVG(e.experience_years),2) as average_years
+FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id=e.employee_id
+GROUP BY p.project_id;
