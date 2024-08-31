91534538a289114c005e7aa517f4e5f9e5cb7a10
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-08-31 10:51:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (32.86%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
-    int func(vector<int>&dp, int ind){
-        if(ind==0) return 1;
-        if(ind==1) return 1;
-        if(dp[ind]!=-1) return dp[ind];
-        int singleStep = func(dp,ind-1);
-        int doubleStep = func(dp,ind-2);
-        return dp[ind] = singleStep+doubleStep;
+    int func(int n, vector<int>&dp){
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=func(n-1,dp)+func(n-2,dp);
     }
-    int climbStairs(int ind) {
-        vector<int> dp(ind+1,-1);
-        return func(dp, ind);
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        return func(n,dp);
     }
 };
