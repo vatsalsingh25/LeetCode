2a3194d7f6f3db92a82120efd1bbb713e7596044
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-09-04 23:25:38 +0530

    Time: 14 ms (31.12%) | Memory: 14 MB (26.23%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    int func(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp){
+    int func(vector<vector<int>>& matrix, int i, int j, vector<vector<int>>&dp){
         int n = matrix.size();
-        if(i>=n || j>=n || j<0) return 1e9;
-        if(i==n-1) return matrix[i][j];
-        if(dp[i][j]!=1e9) return dp[i][j]; //...
-        int down = matrix[i][j] + func(i+1,j,matrix,dp);
-        int dleft = matrix[i][j] + func(i+1,j-1,matrix,dp);
-        int dright = matrix[i][j] + func(i+1,j+1,matrix,dp); 
 
-        return dp[i][j]=min(down,min(dleft,dright)); //...
+        if(j<0 || j>=n) return 1e9;
+        if(i==n-1) return matrix[i][j];
+        // if(j<0 || j>=n) return 1e9;
+
+        if(dp[i][j]!=1e9) return dp[i][j];
+
+        int down = matrix[i][j]+func(matrix,i+1,j,dp);
+        int dleft = matrix[i][j]+func(matrix,i+1,j-1,dp);
+        int dright = matrix[i][j]+func(matrix,i+1,j+1,dp);
+
+        return dp[i][j]=min(down,min(dleft,dright));
     }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<vector<int>>dp(n,vector<int>(n,1e9)); //...
+        vector<vector<int>>dp(n,vector<int>(n,1e9));
         int mini = INT_MAX;
-        for(int i=0; i<n; i++){
-            mini=min(mini, func(0,i,matrix,dp));
+        for(int j=0; j<n; j++){
+            int sum = func(matrix,0,j,dp);
+            mini=min(mini,sum);
         }
         return mini;
     }
-    //addition after recurr...
 };
