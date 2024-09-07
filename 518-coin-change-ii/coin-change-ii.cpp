391f97ba38df8bf7f42af17502397b9d120e6c9d
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-09-07 19:55:27 +0530

    Time: 3 ms (91.73%) | Memory: 20 MB (63.07%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int func(vector<int>&coins, int ind, int amount,vector<vector<int>>&dp){
+    int func(vector<int>&coins, int amount, int ind, vector<vector<int>>&dp){
         if(amount==0) return 1;
         if(ind==0){
-            return (amount%coins[0]==0);
+            if(amount%coins[0]==0) return 1;
+            else return 0;
         }
 
         if(dp[ind][amount]!=-1) return dp[ind][amount];
-        
-        int notPick = func(coins,ind-1,amount,dp);
-        int pick=0;
-        if(amount>=coins[ind]) pick = func(coins,ind,amount-coins[ind],dp);
+
+        int notPick = func(coins,amount,ind-1,dp);
+        int pick = 0;
+        if(amount-coins[ind]>=0) pick = func(coins,amount-coins[ind],ind,dp);
 
         return dp[ind][amount]=pick+notPick;
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return func(coins,n-1,amount,dp);
+        return func(coins,amount,n-1,dp);
     }
 };
