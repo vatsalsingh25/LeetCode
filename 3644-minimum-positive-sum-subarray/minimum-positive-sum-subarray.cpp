ba53b53ba056f7536758dd21ecf795525a883a10
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-11-25 05:42:37 +0530

    Time: 2 ms (58.33%) | Memory: 28.8 MB (25.00%) - LeetSync

---
+++ 3644-minimum-positive-sum-subarray/minimum-positive-sum-subarray.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int func(vector<int>& nums, int k){
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0; i<k; i++){
+            sum+=nums[i];
+        }
+        int left = 0;
+        int right = k-1;
+        int mini = INT_MAX;
+        while(right<n){
+            if(sum>0) mini = min(sum,mini);
+            if(right+1<n) sum = sum+nums[right+1]-nums[left];
+            right++;
+            left++;
+        }
+        return mini;
+    }
+    int minimumSumSubarray(vector<int>& nums, int l, int r) {
+        int ans = INT_MAX;
+        for(int i=l; i<=r; i++){
+            ans = min(ans, func(nums,i));
+        }
+        if(ans == INT_MAX) return -1;
+        return ans;
+    }
+};
