0cc9bee475d23ffd185a6a22bd469597893bde3f
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-11-25 11:55:20 +0530

    Time: 248 ms (100.00%) | Memory: 72.3 MB (57.14%) - LeetSync

--- 3595-rearrange-k-substrings-to-form-target-string/rearrange-k-substrings-to-form-target-string.cpp
+++ 3595-rearrange-k-substrings-to-form-target-string/rearrange-k-substrings-to-form-target-string.cpp
@@ -3,20 +3,14 @@ public:
     bool isPossibleToRearrange(string s, string t, int k) {
         int n = s.size();
         unordered_map<string,int>hash;
-        for(int i=0; i<k; i++){
-            string ss = "";
-            for(int j=n/k*i; j<n/k*i+n/k; j++){
-                ss+=s[j];
-            }
+        for(int i=0; i<n; i+=n/k){                      //loop k times in string s to get k subarrays
+            string ss = s.substr(i,n/k);
             hash[ss]++;
         }
-        for(int i=0; i<k; i++){
-            string ss = "";
-            for(int j=n/k*i; j<n/k*i+n/k; j++){
-                ss+=t[j];
-            }
-            if(hash.find(ss)!=hash.end() && hash[ss]!=0) hash[ss]--;
-            else return false;
+        for(int i=0; i<n; i+=n/k){                      //we will check each substring of t against hashmap of string s
+            string ss = t.substr(i,n/k); 
+            if(hash.find(ss)!=hash.end() && hash[ss]!=0) hash[ss]--;  // if found in hash, decrease count by one 
+            else return false;                                        // if not, return false
         }
         return true;
     }
