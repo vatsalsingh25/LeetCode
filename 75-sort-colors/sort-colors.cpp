0639410bfe21ba7454f6fc632685e78c4dc7b497
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-12-27 18:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (16.08%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,62 +1,18 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1) return;
-        int l = 0, r = 0;
-        while(l<n && r<n){
-            while(l<n && nums[l]==0){
-                l++;
-                r=l+1;
-            } 
-            if(r<n && nums[r]==0){
-                swap(nums[l],nums[r]);
-                l++;
+        int n=nums.size(), left=0, mid=0, right = n-1;
+        while(mid<=right){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[left]);
+                mid++;
+                left++;
             }
-            r++;
-        }
-        l=n-1;
-        r=n-1;
-        while(l>=0 && r>=0){
-            while(l>=0 && nums[l]==2){
-                l--;
-                r=l-1;
-            } 
-            if(r>=0 && nums[r]==2){
-                swap(nums[l],nums[r]);
-                l--;
+            else if(nums[mid]==2){
+                swap(nums[right],nums[mid]);
+                right--;
             }
-            r--;
+            else mid++;
         }
-        return;
     }
 };
-
-// class Solution {
-// public:
-//     void sortColors(vector<int>& nums) {
-//         int n = nums.size();
-//         if (n <= 1) return; // No need to sort if there's only one or no element
-
-//         // Step 1: Sort all 0s to the beginning
-//         int l = 0, r = 0;
-//         while (r < n) {
-//             if (nums[r] == 0) {
-//                 swap(nums[l], nums[r]);
-//                 l++;
-//             }
-//             r++;
-//         }
-
-//         // Step 2: Sort all 2s to the end
-//         l = n - 1;
-//         r = n - 1;
-//         while (r >= 0) {
-//             if (nums[r] == 2) {
-//                 swap(nums[l], nums[r]);
-//                 l--;
-//             }
-//             r--;
-//         }
-//     }
-// };
