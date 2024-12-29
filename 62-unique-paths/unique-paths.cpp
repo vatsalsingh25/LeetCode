ec797bf84b57195858ce24602b7614cbb7e23fac
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-12-29 18:38:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (77.68%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -4,8 +4,8 @@ public:
         int N = m+n-2;            // (m-1) right steps +(n-1) downward steps we must take to reach the end
         int r = m-1;
         double ans = 1;
-        for(int i=0; i<r; i++){
-            ans= ans*(N-r+i+1)/(i+1);
+        for(int i=1; i<=r; i++){
+            ans= ans*(N-r+i)/(i);
         } 
         return (int)ans;
     }
