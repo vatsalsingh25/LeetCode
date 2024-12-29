dfeceb1db5a631d90230df86739e1583a5a5f057
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2024-12-29 18:38:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (77.68%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
-    int func(int m, int n, vector<vector<int>>&dp){
-        if(m==0 && n==0) return 1;
-        if(m<0 || n<0) return 0;
-        
-        if(dp[m][n]!=-1) return dp[m][n];
-
-        int up = func(m-1,n,dp);
-        int left = func(m,n-1,dp);
-
-        return dp[m][n]=up+left;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return func(m-1,n-1,dp);
+        int N = m+n-2;            // (m-1) right steps +(n-1) downward steps we must take to reach the end
+        int r = m-1;
+        double ans = 1;
+        for(int i=0; i<r; i++){
+            ans= ans*(N-r+i+1)/(i+1);
+        } 
+        return (int)ans;
     }
 };
