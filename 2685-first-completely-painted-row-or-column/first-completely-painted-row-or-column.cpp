2f27f638a1382426e9cbf51bf1e59a1f737a9dd6
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-01-21 11:19:47 +0530

    Time: 273 ms (6.03%) | Memory: 213.4 MB (6.02%) - LeetSync

---
+++ 2685-first-completely-painted-row-or-column/first-completely-painted-row-or-column.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+        // make matrix to store locations of elements;
+        int m = mat.size();
+        int n = mat[0].size();
+        unordered_map<int,vector<int>>location;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                location[mat[i][j]] = {i,j};
+            }
+        }
+        int ans;
+        //mark numbers to 0 if traversed and also check if whole column/row is marked 0;
+        vector<int>rfreq(m);
+        vector<int>cfreq(n);
+        for(int i=0; i<arr.size(); i++){
+            vector<int>loc = location[arr[i]];
+            int r = loc[0];
+            int c = loc[1];
+            mat[r][c]=0;
+            rfreq[r]++;
+            cfreq[c]++;
+            if(rfreq[r]==n || cfreq[c]==m) return i;
+        }
+        return -1;
+    }
+};
