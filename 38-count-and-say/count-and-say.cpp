6e82926d94f2fc6407a04ea507c06bf95de85c2e
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-01-29 22:32:42 +0530

    Time: 6 ms (31.91%) | Memory: 9.9 MB (57.57%) - LeetSync

--- 38-count-and-say/count-and-say.cpp
+++ 38-count-and-say/count-and-say.cpp
@@ -19,7 +19,7 @@ public:
     }
     string countAndSay(int n) {
         string s = "1";
-        for(int i=1; i<n; i++){
+        for(int i=0; i<n-1; i++){
             if(n==1) return s;
             s=functionForRLE(s);
         }
