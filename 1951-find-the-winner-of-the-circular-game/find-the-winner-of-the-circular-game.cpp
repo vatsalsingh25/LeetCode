7a7f504066412eca7cc65fc8fbe0081d62849c48
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-02-06 00:15:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (32.67%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
-    int func(int n, int k){
-        if(n==0) return 0;
-        return (func(n-1,k)+k)%n;
-    }
+    // int func(int n, int k){
+    //     if(n==0) return 0;
+    //     return (func(n-1,k)+k)%n;
+    // }
+    // int findTheWinner(int n, int k) {
+    //     return func(n,k)+1; //1 based indexing
+    // }
     int findTheWinner(int n, int k) {
-        return func(n,k)+1; //1 based indexing
+        vector<int>players;
+        for(int i=1; i<=n; i++){
+            players.push_back(i);
+        }
+        int startInd = 0;
+        while(players.size()>1){
+            int removeInd = (startInd+k-1)%players.size();
+            players.erase(players.begin()+removeInd);
+            startInd = removeInd; // since the previous index is deleted, remove index automatically takes next place
+        }
+        return players[0];
     }
 };
