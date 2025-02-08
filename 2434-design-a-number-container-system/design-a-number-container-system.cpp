df67c7958bb1433faf0d2e0c2f048d314eea0155
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-02-08 05:41:00 +0530

    Time: 135 ms (27.81%) | Memory: 195.7 MB (71.56%) - LeetSync

--- 2434-design-a-number-container-system/design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/design-a-number-container-system.cpp
@@ -1,33 +1,33 @@
 class NumberContainers {
     unordered_map<int,int>indexToNum;         //stores the value for the index
-    unordered_map<int,set<int>>numberToIndex;      //stores all the indexes for a value
+    unordered_map<int,set<int>>numberToIndex;      //stores all the indexes for a value, set automatically sort the indexes
 public:
     NumberContainers() {
         
     }
     
     void change(int index, int number) {
-        if(indexToNum.find(index)==indexToNum.end()){
-            indexToNum[index]=number;
-            numberToIndex[number].insert(index);
+        if(indexToNum.find(index)==indexToNum.end()){           //if index not in map ie index empty
+            indexToNum[index]=number;                           //insert value in index
+            numberToIndex[number].insert(index);                //mark index for the value
         }
-        else{
-            int prev = indexToNum[index];
-            numberToIndex[prev].erase(index);
-            if (numberToIndex[prev].empty()) {
+        else{                                                   // number already present at index
+            int prev = indexToNum[index];                      
+            numberToIndex[prev].erase(index);                   // delete the number already present
+            if (numberToIndex[prev].empty()) {                  //if the set for number becomes empty, remove the number
                 numberToIndex.erase(prev);
             }
-            indexToNum[index]=number;
-            numberToIndex[number].insert(index);          
+            indexToNum[index]=number;                          //insert new value in index
+            numberToIndex[number].insert(index);               //mark index for the value
         }
         
     }
     
     int find(int number) {
-        if(numberToIndex.find(number)!=numberToIndex.end()){
-            return *numberToIndex[number].begin();
+        if(numberToIndex.find(number)!=numberToIndex.end()){     // if number present 
+            return *numberToIndex[number].begin();               //return the first(smallest due to set usage) element
         }
-        return -1;
+        return -1;                                               //if not present return -1
     }
 };
 
