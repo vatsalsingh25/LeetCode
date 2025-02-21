e3a489f2601aaa7f33b371405cfa74eb1bc36971
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-02-22 00:07:46 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (52.14%) - LeetSync

--- 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
+++ 3331-minimum-operations-to-exceed-threshold-value-i/minimum-operations-to-exceed-threshold-value-i.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>>minHeap(nums.begin(),nums.end());
         int count = 0;
-        while(minHeap.top()<k){
-            minHeap.pop();
-            count++;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]<k) count++;
         }
         return count;
     }
