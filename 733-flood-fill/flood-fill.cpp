76ff2a5704d48ecf1539740fcc2196bc1e3c6012
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-02-25 19:38:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (98.97%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,28 +1,27 @@
 class Solution {
 private:
-    void dfs(int row, int col, int orgColor,vector<vector<int>>vis, vector<vector<int>>& image, int color){
+    void dfs(int row, int col, int orgColor, vector<vector<int>>& image, int color){
         int m = image.size();
         int n = image[0].size();  
 
-        if(row<0 || row>=m || col<0 || col>=n || image[row][col]!=orgColor || vis[row][col]) return;
+        if(row<0 || row>=m || col<0 || col>=n || image[row][col]!=orgColor) return;
 
-        vis[row][col]=1;
         image[row][col]=color;
 
-        dfs(row-1,col,orgColor,vis,image,color);
-        dfs(row+1,col,orgColor,vis,image,color);
-        dfs(row,col-1,orgColor,vis,image,color);
-        dfs(row,col+1,orgColor,vis,image,color);
+        dfs(row-1,col,orgColor,image,color);
+        dfs(row+1,col,orgColor,image,color);
+        dfs(row,col-1,orgColor,image,color);
+        dfs(row,col+1,orgColor,image,color);
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m = image.size();
         int n = image[0].size();
 
-        vector<vector<int>>vis(m,vector<int>(n,0));
-
         int orgColor = image[sr][sc];
-        dfs(sr,sc,orgColor,vis,image,color);
+        if(orgColor == color) return image; 
+        
+        dfs(sr,sc,orgColor,image,color);
 
         return image;
     }
