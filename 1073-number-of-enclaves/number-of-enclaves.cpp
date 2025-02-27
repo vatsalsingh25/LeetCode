e3ee0b924053bfa05141559676fdc8094c0e09fc
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-02-28 03:22:12 +0530

    Time: 12 ms (37.30%) | Memory: 31.8 MB (69.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,43 +1,63 @@
 class Solution {
-private:
-    void dfs(int row, int col, vector<vector<int>>& grid, vector<vector<int>>&vis){
-        int m = grid.size();
-        int n = grid[0].size();
-
-        if(row<0 || row>=m || col<0 || col>=n || grid[row][col]==0 || vis[row][col]) return;
-
-        vis[row][col]=1;
-
-        dfs(row-1,col,grid,vis);  
-        dfs(row+1,col,grid,vis);
-        dfs(row,col-1,grid,vis);
-        dfs(row,col+1,grid,vis);      
-    }
 public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
 
         vector<vector<int>>vis(m,vector<int>(n,0));
+        queue<pair<int,int>>q;
 
-        //traverse top and bottom boundry
+        // traverse all the boundries and push all '1' points into queue
         for(int j=0; j<n; j++){
-            if(grid[0][j]==1 && !vis[0][j]) dfs(0,j,grid,vis);
-            if(grid[m-1][j]==1 && !vis[m-1][j]) dfs(m-1,j,grid,vis);
+            if(grid[0][j]==1){
+                vis[0][j]=1; 
+                q.push({0,j});
+            } 
+            if(grid[m-1][j]==1){
+                vis[m-1][j]=1;
+                q.push({m-1,j});
+            } 
         }
-        //traverse right and left boundry
         for(int i=0; i<m; i++){
-            if(grid[i][0]==1 && !vis[i][0]) dfs(i,0,grid,vis);
-            if(grid[i][n-1]==1 && !vis[i][n-1]) dfs(i,n-1,grid,vis);
+            if(grid[i][0]==1){
+                vis[i][0]=1; 
+                q.push({i,0});
+            } 
+            if(grid[i][n-1]==1){
+                vis[i][n-1]=1; 
+                q.push({i,n-1});
+            } 
         }
 
-        //count all the 1s in the grid that is not marked in the vis and return the count
+        //queue operations, pop front element and traverse all four direction 
+        //and add new element if required node is found
+        vector<int>delr = {-1,1,0,0};
+        vector<int>delc = {0,0,-1,1};
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i=0; i<4; i++){
+                int nrow = row+delr[i];
+                int ncol = col+delc[i];
+
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && 
+                grid[nrow][ncol]==1 && !vis[nrow][ncol]){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+
         int count = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(grid[i][j]==1 && vis[i][j]==0) count++;
             }
-        } 
+        }
+
         return count;
+
     }
 };
