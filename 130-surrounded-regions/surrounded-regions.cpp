a7f0e3967a04dadbae3f98f119ee598a9906c71b
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-02-27 13:09:03 +0530

    Time: 3 ms (27.46%) | Memory: 14.6 MB (38.72%) - LeetSync

---
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,48 @@
+class Solution {
+private:
+    void dfs(int row, int col, vector<vector<char>>& board, vector<vector<int>>&vis){
+        int m = board.size();
+        int n = board[0].size();
+
+        if(row<0 || row>=m || col<0 || col>=n || vis[row][col] || board[row][col]!='O') return; 
+
+        vis[row][col]=1;    
+
+        dfs(row-1,col,board,vis);   
+        dfs(row+1,col,board,vis); 
+        dfs(row,col-1,board,vis); 
+        dfs(row,col+1,board,vis); 
+    }
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>>vis(m,vector<int>(n,0));
+        
+        //traverse top and bottom boundry
+        for(int j=0; j<n; j++){
+            if(board[0][j]=='O') dfs(0,j,board,vis);
+            if(board[m-1][j]=='O') dfs(m-1,j,board,vis);
+        }
+
+
+        //traverse top and bottom boundry
+        for(int i=0; i<m; i++){
+            if(board[i][0]=='O') dfs(i,0,board,vis);
+            if(board[i][n-1]=='O') dfs(i,n-1,board,vis);
+        }
+
+
+        // visited array is complete after above lines are executed
+        // change all the elements to 'X' in board, 
+        //except those marked as 1 in vis(ie boundry O and connectd)
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(vis[i][j]==1) continue;
+                else board[i][j]='X'; 
+            }
+        }
+
+        return;
+    }
+};
