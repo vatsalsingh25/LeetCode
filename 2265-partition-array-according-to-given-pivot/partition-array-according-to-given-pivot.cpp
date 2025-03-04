8397cced778d6708dddb94165d25f35a0bf9423f
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-03-04 10:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 127.9 MB (71.33%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        vector<int>less;
-        vector<int>more;
-        vector<int>equal;
+        //using two pointer
+        int indLess = 0;
+        int indMore = nums.size()-1;
+        vector<int>ans(nums.size(),1e7);
         for(int i=0; i<nums.size(); i++){
-            if(nums[i]<pivot) less.push_back(nums[i]);
-            else if(nums[i]>pivot) more.push_back(nums[i]);
-            else equal.push_back(nums[i]);
+            if(nums[i]<pivot){
+                ans[indLess]=nums[i];
+                indLess++;
+            }
         }
-        vector<int>ans;
-        for(int i=0; i<less.size(); i++){
-            ans.push_back(less[i]);
+        for(int i=nums.size()-1; i>=0; i--){
+            if(nums[i]>pivot){
+                ans[indMore]=nums[i];
+                indMore--;
+            }           
         }
-        for(int i=0; i<equal.size(); i++){
-            ans.push_back(equal[i]);
-        }
-        for(int i=0; i<more.size(); i++){
-            ans.push_back(more[i]);
+        for(int i=0; i<nums.size(); i++){
+            if(ans[i]==1e7) ans[i]=pivot;
         }
         return ans;
     }
