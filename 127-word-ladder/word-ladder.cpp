f06fd3a88f9e9b39ed9a654a68b3de7aba6116ed
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-03-07 02:20:07 +0530

    Time: 362 ms (26.16%) | Memory: 21.3 MB (46.29%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -11,13 +11,13 @@ public:
             string s = q.front().first;
             int num = q.front().second;
             q.pop();
-
+            if(s==endWord) return num;
             for(int i=0; i<s.size(); i++){
                 for(int j=0; j<26; j++){  
                     string temp = s;  
                     temp[i]='a'+j;
                     if(st.find(temp)!=st.end()){
-                        if(temp==endWord) return num+1;
+                        
                         q.push({temp,num+1});
                         st.erase(temp);
                     }
