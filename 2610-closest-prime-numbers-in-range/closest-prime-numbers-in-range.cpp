60c24694a1d58a004c2a17d39429824eac47f341
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-03-07 16:27:31 +0530

    Time: 2056 ms (5.18%) | Memory: 8.1 MB (96.48%) - LeetSync

---
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isPrime(int num){
+        if(num==1) return false;
+        for(int i=2; i<=sqrt(num); i++){
+            if(num%i==0) return false;
+        }
+        return true;
+    }
+
+    vector<int> closestPrimes(int left, int right) {
+        int prev = -1;
+        int a = -1;
+        int b = -1;
+        int minDiff = INT_MAX;
+
+        for(int i=left; i<=right; i++){
+            if(isPrime(i)){
+                if(prev!=-1){
+                    int diff = i-prev;
+                    if(diff<minDiff){
+                        minDiff = diff;
+                        a = prev;
+                        b = i;
+                    }
+                }
+                prev = i;
+            } 
+        }
+
+        return {a,b};
+    }
+};
