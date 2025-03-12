4cc26380676ea535f410246ee91b03072cfe7247
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-03-13 04:39:35 +0530

    Time: 0 ms (100.00%) | Memory: 21.4 MB (75.95%) - LeetSync

--- 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
+    int binarySearch(vector<int>& nums, int k){
+        int n = nums.size();
+        int l = 0;
+        int h = n-1;
+
+        while(l<=h){
+            int mid = (l+h)/2;
+            if(nums[mid]>=k) h = mid-1;
+            else l = mid+1;
+        }
+        return l;
+    }
     int maximumCount(vector<int>& nums) {
         int n = nums.size();
-        int pos=0, neg=0;
-        for(int i=0; i<n; i++){
-            if(nums[i]>0) pos++;
-            if(nums[i]<0) neg++;
-        }
+        int pos = n-binarySearch(nums, 1);
+        int neg = binarySearch(nums,0);
         return max(pos,neg);
     }
 };
