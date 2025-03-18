e953f76d9682efc7eaa9ed75acddd5ce08292b88
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-03-19 04:00:25 +0530

    Time: 0 ms (100.00%) | Memory: 61 MB (63.16%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/longest-nice-subarray.cpp
@@ -19,3 +19,12 @@ public:
         return maxi;
     }
 };
+// the main logic behind this problem is,
+// how can we know that each and every number satissfy the & condition with the new number so that we can add it in sliding window
+// what we can do is carry forward all the set bits
+//eg 1->00000001 curr = 00000001
+//   3->00000011 now we seee that curr & 3 is not 0, hence we shrink window and unset 1 and curr becomes 00000010
+//   8->00001000 curr & 8 = 0, valid hence we set curr = 00001011 so that we can check ahead
+//  48->00101010 curr & 48 = 0, valid hence we set curr = 00101011, simnultaneously we also update maximum length
+//  10->00000110 curr & 10 != 0, shrink until valid and unset curr values simulaneously as yo shrink
+// now if we encounted   
