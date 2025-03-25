29f05c872a66e1bb7059b9c818fbb480891723b2
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-03-25 14:17:04 +0530

    Time: 391 ms (30.64%) | Memory: 302.5 MB (26.26%) - LeetSync

--- 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
+++ 3657-check-if-grid-can-be-cut-into-sections/check-if-grid-can-be-cut-into-sections.cpp
@@ -1,41 +1,50 @@
 class Solution {
 public:
     bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        //creating a (xStart,xEnd) array for allowing vertical lines
+        //creating a (yStart,yEnd) array for allowing horizontal lines
         vector<vector<int>>xAxis;
         vector<vector<int>>yAxis;
         for(auto it: rectangles){
             xAxis.push_back({it[0],it[2]});
             yAxis.push_back({it[1],it[3]});
         }
-        
+
+        //sorting the array to get the axes together for furthur operation
         sort(xAxis.begin(), xAxis.end());
         sort(yAxis.begin(), yAxis.end());\
 
-        int latestEndy = yAxis[0][1];
-        int yCount = 0;
+        //checking if horizontal seperation is possible
+
+        // set the previous END of y = end point fo first rectangle 
+        int perviousEndY = yAxis[0][1];
+        int countY = 0;
+        //start loop from index 1 as thats where we can start considering 
+        //division as each part should have atleast one recangle
         for(int i=1; i<yAxis.size(); i++){
             int start = yAxis[i][0];
             int end = yAxis[i][1];
-
-            if(start>=latestEndy){
-                yCount++;
+            //if current point is above or equal to the previous end point, we can divide the plane
+            if(start>=perviousEndY){
+                countY++;
             }
-            latestEndy = max(end,latestEndy);
+            perviousEndY = max(end,perviousEndY);
         }
-        if(yCount>=2) return true;
+        if(countY>=2) return true;
 
-        int latestEndx = xAxis[0][1];
-        int xCount = 0;
+        //checking if vertical seperation is possible (exact approach as above)
+        int previousEndX = xAxis[0][1];
+        int countX = 0;
         for(int i=1; i<xAxis.size(); i++){
             int start = xAxis[i][0];
             int end = xAxis[i][1];
 
-            if(start>=latestEndx){
-                xCount++;
+            if(start>=previousEndX){
+                countX++;
             }
-            latestEndx = max(end,latestEndx);
+            previousEndX = max(end,previousEndX);
         }
-        if(xCount>=2) return true;
+        if(countX>=2) return true;
 
         return false;
     }
