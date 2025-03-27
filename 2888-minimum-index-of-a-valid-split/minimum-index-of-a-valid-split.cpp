9e3acee608b4a7b6472a5b3a79d333c9f8d356a7
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-03-28 02:36:32 +0530

    Time: 53 ms (32.78%) | Memory: 99.2 MB (36.93%) - LeetSync

---
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimumIndex(vector<int>& nums) {
+        int n = nums.size();
+        map<int,int>freq;
+        int dominant = 0;
+        for(int i=0; i<n; i++){
+            freq[nums[i]]++;
+        }
+        for(auto it: freq){
+            if(it.second>n/2) dominant = it.first;
+        }
+        if(dominant==0) return -1;
+
+        int count = freq[dominant];
+        
+        int countDomL = 0;
+        int countDomR = count;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]==dominant){
+                countDomL++;
+                countDomR--;
+            }
+            int arrHalfL = (i+1)/2;
+            int arrHalfR = (n-i-1)/2;
+            if(countDomL>arrHalfL && countDomR>arrHalfR) return i;
+        }
+        return -1;
+        
+    }
+};
