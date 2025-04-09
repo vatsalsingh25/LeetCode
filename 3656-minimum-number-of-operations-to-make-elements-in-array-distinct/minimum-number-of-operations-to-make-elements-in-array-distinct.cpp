7b1cd91be96b0b28824fd6eeef357c436843c9c3
vatsalsingh25 <133035358+vatsalsingh25@users.noreply.github.com>
2025-04-09 15:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (66.27%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -2,46 +2,14 @@ class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int>hash;
-        for(int i=0; i<n; i++){
-            hash[nums[i]]++;
-        }
-        int alreadySorted = true;
-        for(auto it: hash){
-            if(it.second>1){
-                alreadySorted = false;
-                break;
+        map<int,int>hash;
+        for(int i=n-1; i>=0; i--){
+            if(hash[nums[i]]>=1){
+                if((i+1)%3==0) return (i+1)/3;
+                else return ((i+1)/3)+1; 
             }
+            else hash[nums[i]]++;
         }
-        if(alreadySorted) return 0;
-
-        int count = 0;
-        int i=0;
-        while(true){
-            count++;
-            bool outOfBoundFlag = false;
-            for(int j=i; j<i+3; j++){
-                if(j<n) hash[nums[j]]--;
-                else{
-                    outOfBoundFlag = true;
-                    break;
-                } 
-            }
-            if (outOfBoundFlag) break;
-            bool duplicateFlag = false;
-            for(auto it: hash){
-                if(it.second>1){
-                    duplicateFlag = true;
-                    break;
-                }
-            }
-            if(duplicateFlag){
-                i+=3;
-                continue;
-            } 
-            else break;
-            
-        }
-        return count;
+        return 0;
     }
 };
